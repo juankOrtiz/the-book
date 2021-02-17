dddfcbb706ca591f48639aa179e95947bf934c11
juankOrtiz <juank.ortiz.1991gmail.com>
2021-02-17 20:49:18 -0300

    Compilar proyecto con cargo

---
+++ hola_cargo/target/debug/deps/hola_cargo.d
@@ -0,0 +1,5 @@
+E:\Proyectos\Rust\the-book\hola_cargo\target\debug\deps\hola_cargo.exe: src\main.rs
+
+E:\Proyectos\Rust\the-book\hola_cargo\target\debug\deps\hola_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hola_cargo/target/debug/hola_cargo.d
@@ -0,0 +1,1 @@
+E:\Proyectos\Rust\the-book\hola_cargo\target\debug\hola_cargo.exe: E:\Proyectos\Rust\the-book\hola_cargo\src\main.rs
